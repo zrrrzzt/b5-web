03011dbee928327b4159e9216912096217b91e38
Geir Gåsodden <geir.gasodden@pythonia.no>
2018-02-20 18:51:39 +0100

    Clean up summary

--- components/Summary.js
+++ components/Summary.js
@@ -1,10 +1,27 @@
+const SummaryItem = ({ item }) => (
+  <div>
+    <h2>{item.title} - {item.scoreText}</h2>
+    {item.facets.map(facet => <span><strong>{facet.title}</strong> - {facet.scoreText}</span>)}
+    <style jsx>
+      {`
+        span {
+          margin-right: 10px;
+        }
+      `}
+    </style>
+  </div>
+)
+
 export default ({ data }) => (
   <div className={'summary-wrapper'} >
     {data
-    ? data.map(item => <div>{item.title}<p>{item.scoreText}</p></div>)
+    ? data.map(item => <SummaryItem item={item} />)
     : null}
     <style jsx>
       {`
+        span {
+          margin-right: 10px;
+        }
         .summary-wrapper {
           border-radius: 0;
           background-color: #FFF;
@@ -12,11 +29,9 @@ export default ({ data }) => (
           color: black;
           margin-top: 10px;
           padding: 10px;
-          text-align: center;
-          display: flex;
-          justify-content: space-evenly;
+          text-align: left;
         }
-        @media screen and (max-width: 900px) {
+        @media screen and (max-width: 1000px) {
           .summary-wrapper {
             flex-direction: column;
           }
