fa8fc70756b7a6f64ac58966d347ad086da00c56
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-07-15 17:53:50 +0200

    Updates for next-offline v4 (patch)

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
     runtimeCaching: [
       {
         urlPattern: /^https?.*/,
-        handler: 'networkFirst',
+        handler: 'NetworkFirst',
         options: {
           cacheName: 'https-calls',
           networkTimeoutSeconds: 15,
