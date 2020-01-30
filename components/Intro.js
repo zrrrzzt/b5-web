815df8804a3b675693210e6585a44c751584c487
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-01-30 19:52:15 +0100

    Sorts languages (patch)

--- components/Intro.js
+++ components/Intro.js
@@ -8,6 +8,7 @@ const SelectLanguage = props => {
     setSelectedOption(lang)
     setLanguage(lang)
   }
+  languages.sort((a, b) => (a.text > b.text) ? 1 : -1)
   return (
     <select onChange={handleLanguageSelect} value={selectedOption} className='text-xl border border-black bg-blue-100 block appearance-none w-full bg-gray-200 border border-gray-200 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-yellow-300 focus:border-gray-500'>
       {languages.map((lang, index) => <option value={lang.id} key={index}>{lang.text}</option>)}
