1543795f1fecb6df3317c8f82b8d0cdac17a8a71
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-02-08 20:41:00 +0100

    Adds upload button (patch)

--- components/LoadFile.js
+++ components/LoadFile.js
@@ -5,8 +5,13 @@ function triggerUpload (e) {
 }
 
 export default ({ handler, buttonTitle }) => (
-  <div className='flex'>
-    <input type='file' accept='.json' onChange={handler} />
-    <button onClick={triggerUpload} className='border border-black mt-4 p-4 text-2xl bg-teal-200 hover:bg-yellow-300 shadow w-full'>{buttonTitle}</button>
+  <div class='flex'>
+    <label class='w-64 flex flex-col items-center px-4 py-6 bg-white text-blue rounded-lg shadow-lg tracking-wide uppercase border border-blue cursor-pointer hover:bg-blue hover:text-teal-600'>
+      <svg class='w-8 h-8' fill='currentColor' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
+        <path d='M16.88 9.1A4 4 0 0 1 16 17H5a5 5 0 0 1-1-9.9V7a3 3 0 0 1 4.52-2.59A4.98 4.98 0 0 1 17 8c0 .38-.04.74-.12 1.1zM11 11h3l-4-4-4 4h3v3h2v-3z' />
+      </svg>
+      <span class='mt-2 text-base leading-normal' onClick={triggerUpload}>{buttonTitle}</span>
+      <input type='file' accept='.json' onChange={handler} class='hidden' />
+    </label>
   </div>
 )
