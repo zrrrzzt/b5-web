f3b96775c7cfacd4ab34d4e4bfe5564d107e1d54
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-02-08 08:14:45 +0100

    Fixes (kind of) navbar (patch)

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,28 +1,23 @@
 import Link from 'next/link'
 
 export default () => (
-  <nav>
-    <ul className='left'>
-      <li>
-        <Link href='/'>
-          <a>
-            <img style={{ width: '32px' }} src='/static/favicon-32x32.png' />
-          </a>
-        </Link>
-      </li>
-      <li><span className='app-name'>Big Five Test</span></li>
-    </ul>
-    <ul className='right'>
-      <li>
+  <nav className='flex items-center justify-between flex-nowrap bg-black p-6'>
+    <div className='flex items-center text-white mr-6'>
+      <Link href='/'>
+        <a>
+          <img className='w-8' src='/static/favicon-32x32.png' />
+        </a>
+      </Link>
+    </div>
+    <div className='w-full block flex items-center w-auto'>
+      <div className='text-xl flex flex-nowrap'>
         <Link href='/result'>
-          <a>Result</a>
+          <a className='block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4'>Result</a>
         </Link>
-      </li>
-      <li>
         <Link href='/compare'>
-          <a>Compare</a>
+          <a className='block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4'>Compare</a>
         </Link>
-      </li>
-    </ul>
+      </div>
+    </div>
   </nav>
 )
