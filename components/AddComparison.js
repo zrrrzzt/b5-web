16c283802854291cd5a3c2cae562a77e4a26847d
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-02-08 20:58:26 +0100

    More form fixes (patch)

--- components/AddComparison.js
+++ components/AddComparison.js
@@ -1,9 +1,11 @@
 export default ({ addComparison }) => (
-  <form onSubmit={addComparison}>
-    <div className='flex flex-col'>
-      <input type='text' id='comparisonName' placeholder='Name for comparison' className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-4' required />
-      <input type='text' id='comparisonData' placeholder='URL or id for comparison' className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mt-4' required />
-      <button type='submit' className='border border-black mt-4 p-4 text-2xl bg-teal-200 hover:bg-yellow-300 w-full shadow'>Add</button>
-    </div>
-  </form>
+  <div className='container'>
+    <form onSubmit={addComparison} className='bg-teal-200 text-center px-3 py-4 text-black mx-auto rounded mt-4'>
+      <input type='text' id='comparisonName' placeholder='Name for comparison' className='block w-full mx-auto text-sm py-2 px-3 rounded' />
+      <input type='text' id='comparisonData' placeholder='URL or id for comparison' className='block w-full mx-auto text-sm py-2 px-3 rounded my-3' />
+      <button className='bg-yellow-300 hover:bg-white text-black font-bold py-2 px-4 rounded border block mx-auto w-full' type='submit'>
+        Add
+      </button>
+    </form>
+  </div>
 )
