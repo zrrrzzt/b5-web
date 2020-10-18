51a385255142a213110fc7ed85c29c27771c81a4
Geir Gåsodden <geir.gasodden@pythonia.no>
2020-10-18 08:20:26 +0200

    Refactoring (patch)

--- components/Resume.js
+++ components/Resume.js
@@ -3,12 +3,8 @@ import Domain from './Domain'
 
 const Resume = ({ data, width }) => (
   <div>
-    {data
-      ? <Summary data={data} yDomainRange={[24, 120]} chartWidth={width} />
-      : null}
-    {data
-      ? data.map((domain, index) => <Domain data={domain} key={index} chartWidth={width} />)
-      : null}
+    {data && <Summary data={data} yDomainRange={[24, 120]} chartWidth={width} />}
+    {data && data.map((domain, index) => <Domain data={domain} key={index} chartWidth={width} />)}
   </div>
 )
 
